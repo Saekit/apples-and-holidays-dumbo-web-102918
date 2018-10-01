df30884c89b003eff9cbfa25872c9668494bfdb8
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-01 21:50:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,17 +64,17 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  # holiday_hash.collect do |season, holiday|
+  holiday_hash.collect do |season, holiday|
     
-  #   holiday.collect do |holi, supply|
-  #     bbqHolidays = []
-  #     if supply.include?("BBQ")
-  #       bbqHolidays << holi
-  #     end
-  #     if bbqHolidays.
-  #   end
+    holiday.collect do |holi, supply|
+      bbqHolidays = []
+      if supply.include?("BBQ")
+        bbqHolidays << holi
+      end
+      if bbqHolidays.
+    end
     
-  # end
+  end
   
   
 end
