f8c9e116e56a5900b4282448d7b8638eeea75b76
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-01 21:51:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     
     holiday.collect do |holi, supply|
-      bbqHolidays = []
+      
       if supply.include?("BBQ")
         bbqHolidays << holi
       end
